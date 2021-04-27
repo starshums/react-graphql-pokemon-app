5755860e6377efe0f97ad16ea4e8c7dd1e7042b4
CHEMS ED-DINE RACHIDI <chemsed.dinerachidi@gmail.com>
2021-04-27 18:18:09 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-import { Routes } from "./routes";
+import { Routes } from "./routes/index";
 
 function App() {
   const client = new ApolloClient({
